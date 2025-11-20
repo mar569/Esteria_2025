5817aa67d6b1c670197dfd8be04ce283a9cc8bfe
mar2025 <efimin01@icloud.com>
2025-11-20 12:13:28 +0300

    loader

--- src/App.tsx
+++ src/App.tsx
@@ -60,14 +60,7 @@ function App() {
                     <div className="dot"></div>
                     <div className="outline"></div>
                   </div>
-                  <div className="circle">
-                    <div className="dot"></div>
-                    <div className="outline"></div>
-                  </div>
-                  <div className="circle">
-                    <div className="dot"></div>
-                    <div className="outline"></div>
-                  </div>
+
                 </div>
               </div>
             }>
